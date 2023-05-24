510065c59a7df1a8e5538cfda6e01873737bb4c7
Kaina Cockett <kainacockett@gmail.com>
2023-05-24 09:16:23 -0600

    feature: Student created

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -3,8 +3,8 @@ require './lib/student'
 
 RSpec.describe Student do
     describe '#initialize' do
-        describe 'creates an instance of Student with two parameters and default @scores' do
-            student = Student.new({name: Morgan, age: 21})
+        it 'creates an instance of Student with two parameters and default @scores' do
+            student = Student.new({name: "Morgan", age: 21})
 
             expect(student.name).to eq("Morgan")
             expect(student.age).to eq(21)
