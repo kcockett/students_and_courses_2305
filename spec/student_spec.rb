23fd50f1db008d4c5acbbcf873dc4b4ed70260b2
Kaina Cockett <kainacockett@gmail.com>
2023-05-24 09:10:21 -0600

    test: Student

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,5 +2,13 @@ require 'rspec'
 require './lib/student'
 
 RSpec.describe Student do
-  
+    describe '#initialize' do
+        describe 'creates an instance of Student with two parameters and default @scores' do
+            student = Student.new({name: Morgan, age: 21})
+
+            expect(student.name).to eq("Morgan")
+            expect(student.age).to eq(21)
+            expect(student.scores).to eq([])
+        end
+    end
 end
