0c2157ab0d3991cfb37a98fa0bac8eff39ea0efa
Kaina Cockett <kainacockett@gmail.com>
2023-05-24 09:39:20 -0600

    test: Course

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -3,5 +3,13 @@ require './lib/course'
 require './lib/student'
 
 RSpec.describe Course do 
+    describe '#initialize' do
+        it 'should create Course with name, capacity, and empty array of students' do
+            course = Course.new("Calculus", 2)
 
+            expect(course.name).to eq("Calculus")
+            expect(course.capacity).to eq(2)
+            expect(course.students).to eq([])
+        end
+    end
 end
